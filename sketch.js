2b70a01c5a318bcc6fe983b786860ce4b14caefb
Mcluky <luk.meili@bluewin.ch>
2018-08-31 12:55:21 +0200

    merge conflict solved

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 const constWidth = 1200;
-<<<<<<< HEAD
 const constHeight = 700
 var canvas;
 
@@ -8,9 +7,6 @@ var state;
 const DO_NOTHING = 0;
 const RESET = -1;
 const CIRCLE_MODE = 1;
-=======
-const constHeight = 700;
->>>>>>> bc10de55608631c940d02528c56671d054affbe2
 
 function setup(){
     canvas = createCanvas(constWidth, constHeight, WEBGL);
