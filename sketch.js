6e7e65d8895925c02fd9fa85ab2d244c51e1a639
Mcluky <luk.meili@bluewin.ch>
2018-08-31 10:10:38 +0200

    inital upload sketch and html file

---
+++ sketch.js
@@ -0,0 +1,12 @@
+const constWidth = 1200;
+const constHeight = 700
+function setup(){
+    canvas = createCanvas(constWidth, constHeight);
+    canvas.parent("canvas");
+    angleMode(DEGREES);
+    frameRate(60);
+}
+
+function draw(){
+    background('#1e1e1e');
+}
