aeebe1a085249a9fb3589161c4c0f967c9d8472f
Mcluky <luk.meili@bluewin.ch>
2018-09-14 12:51:38 +0200

    added createArray function

--- sketch.js
+++ sketch.js
@@ -162,3 +162,14 @@ var DrawCirle = function (x0, y0, radius) {
     }
 };
 
+function createArray(length) {
+    var arr = new Array(length || 0),
+      i = length;
+  
+    if (arguments.length > 1) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      while (i--) arr[length - 1 - i] = createArray.apply(this, args);
+    }
+  
+    return arr;
+}
